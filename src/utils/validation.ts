2ba555dad4282e5887edec60faaae89ba7e2e317
Dat Vo <dat.vo@hikoni.com>
2023-12-13 09:04:23 +0700

    Fix husky

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,9 +1,7 @@
 import express from 'express';
 import { ValidationChain, validationResult } from 'express-validator';
 import { RunnableValidationChains } from 'express-validator/src/middlewares/schema';
-// can be reused by many routes
 
-// sequential processing, stops running validations chain if the previous one fails.
 export const validate = (validations: RunnableValidationChains<ValidationChain>) => {
   return async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     await validations.run(req);
